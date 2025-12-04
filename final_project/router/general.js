c98eb4d640c55332a8cb0492a6f69b887775a70b
dongbin300 <dongbin300@gmail.com>
2025-12-03 21:59:07 -0500

    1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,27 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify({books}, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books[req.params.isbn], null, 4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(Object.values(books).filter((b)=>b.author === req.params.author), null, 4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(Object.values(books).filter((b)=>b.title === req.params.title), null, 4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(books[req.params.isbn].reviews);
 });
 
 module.exports.general = public_users;
